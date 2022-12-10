d670385e93db783f2e8ec2606d08363d83bd8c24
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-12-10 09:44:41 +0000

    [Add]admin・public/controller,admin・public/view作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :teachers
+# 先生用
+# URL /teachers/sign_in ...
+devise_for :teachers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
