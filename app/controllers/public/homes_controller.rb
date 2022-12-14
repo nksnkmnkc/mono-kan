a15536e03d4ba2a1f318d2b00a9d7277726465ae
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-12-14 09:33:40 +0000

    [Fix]トップページ・マイページ

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,7 @@
 class Public::HomesController < ApplicationController
  def top
-
+  #@items = Item.all.utilization(id: "DESC")
+  #@items = Item.limit(4).utilization("created_at DESC")
+  @genres = Genre.all
  end
 end
