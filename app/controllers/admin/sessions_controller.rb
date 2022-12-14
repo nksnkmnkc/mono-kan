b5f0056e41a612d34f53535457647ba449c5297b
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-12-14 07:11:14 +0000

    [Add]管理者ログイン時のフラッシュメッセージ

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -27,11 +27,13 @@ class Admin::SessionsController < Devise::SessionsController
    protected
 
   def after_sign_in_path_for(resource)
-    admin_path
+    flash[:notice] = "管理者ページにログインしました"
+    admin_root_path
   end
 
 
   def after_sign_out_path_for(resource)
+    flash[:notice] = "管理者ページにログアウトしました"
     new_admin_session_path
   end
 
