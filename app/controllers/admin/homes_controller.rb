b44a87d9855cb4ecf8b2e117f07add613645e86c
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-12-17 08:44:01 +0000

    [Fix]管理者側トップ（使用報告一覧）表示

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,7 +2,6 @@ class Admin::HomesController < ApplicationController
  before_action :authenticate_admin!
 
  def top
-  #@utilizations = Utilization.all
   @utilizations = Utilization.page(params[:page])
  end
 
